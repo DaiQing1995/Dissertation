433d57374d5aae7a65726900b3d1fc3f8bb341f8
DaiQing <dq_199510@163.com>
2019-12-29 19:01:31 +0800

    just the path change

--- wordnet_extend/wordnet_taggen.py
+++ wordnet_extend/wordnet_taggen.py
@@ -6,7 +6,7 @@ import numpy as np
 class WordNetTagGenerator:
 
     def __init__(self):
-        self.model = gensim.models.KeyedVectors.load_word2vec_format("/home/qingdai/Documents/GoogleNews-vectors-negative300.bin.gz", binary=True)
+        self.model = gensim.models.KeyedVectors.load_word2vec_format("D:\\TreatiseWP\\GoogleNews-vectors-negative300.bin.gz", binary=True)
         s = "Concurrent therapy with ORENCIA and TNF antagonists is not recommended"
         token = word_tokenize(s)
         print(token)
@@ -19,8 +19,8 @@ class WordNetTagGenerator:
                 vec.append(np.zeros(self.model.vector_size))
         vec = np.asarray(vec)
         for i in vec:
-            print(np.dot(vec[3], i) / np.sqrt(200))  # 第三个词和其他词相互关系
-            print(np.dot(vec[5], i) / np.sqrt(200))
-            print(np.dot(vec[6], i) / np.sqrt(200))
+            print(np.dot(vec[3], i) / np.sqrt(300))  # 第三个词和其他词相互关系
+            print(np.dot(vec[5], i) / np.sqrt(300))
+            print(np.dot(vec[6], i) / np.sqrt(300))
 
 WordNetTagGenerator()
