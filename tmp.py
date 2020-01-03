7d4432ee3e0a0e98555973dcf51b52eff004a935
DaiQing <dq_199510@163.com>
2020-01-03 10:22:41 +0800

    wordnet install finished

--- tmp.py
+++ tmp.py
@@ -1,3 +1,14 @@
 from operator import itemgetter
 
-dict = {"a":1.5434, "b":0.9}
+# dict = {"a":1.5434, "b":0.9}
+
+def delete_duplicate_data():
+    data = [('derivative', 0.037411973398779656), ('complex value', 0.03524804177545692), ('application', 0.031174833590946405), ('issue', 0.02775668250516399), ('device', 0.02378854625550661), ('device', 0.02378854625550661), ('factor', 0.020099666641670476), ('factor', 0.020099666641670476), ('factor', 0.020099666641670476), ('relational database', 0.019092689295039166)]
+    concepts = set()
+    ret = []
+    for d in data:
+        if d[0] in concepts:
+            continue
+        ret.append(d)
+        concepts.add(d[0])
+    print(ret)
--- wordnet_extend/wordnet_taggen.py
+++ wordnet_extend/wordnet_taggen.py
@@ -1,6 +1,76 @@
 from nltk import word_tokenize
+from nltk.corpus import wordnet as wn
 import gensim
 import numpy as np
+
+
+class WordNetUtils:
+
+    @staticmethod
+    def get_synset(word):
+        # wn.synsets('dog')
+        return wn.synsets(word)
+
+    @staticmethod
+    def get_definition(word):
+        """
+        :param word:
+        :return: the definition of wordset which the input word belongs to
+        """
+        return wn.synset(word).definition()
+
+    @staticmethod
+    def get_example_of_word(word):
+        return wn.synset(word).examples()
+
+    @staticmethod
+    def get_synset_by_pos(word, pos):
+        """
+        :param word:
+        :param pos:NOUN,VERB,ADJ,ADV…
+        :return: synset
+        """
+        if pos == "NOUN":
+            return wn.synsets(word, pos=wn.NOUN)
+
+    @staticmethod
+    def get_words_of_same_synset(word):
+        return wn.synset(word).lemma_names()
+
+    @staticmethod
+    def get_synset_words_of_same_synset(word):
+        return wn.synset(word).lemmas( )
+
+    @staticmethod
+    def get_antonyms(word):
+        good = wn.synset(word)
+        return good.lemmas()[0].antonyms()
+
+    @staticmethod
+    def get_similarity(word1, word2):
+        """
+        值得注意的是，名词和动词被组织成了完整的层次式分类体系，形容词和副词没有被组织成分类体系，所以不能用path_distance。
+形容词和副词最有用的关系是similar to。
+        :param word1:
+        :param word2:
+        :return:
+        """
+        synset1 = wn.synset(word1)
+        synset2 = wn.synset(word2)
+        return  synset1.path_similarity(synset2)
+
+    @staticmethod
+    def get_entailments(word):
+        return wn.synset(word).entailments()
+
+    @classmethod
+    def test_all(cls, word):
+        print(cls.get_definition(word))
+        # print(cls.get_entailments(word))
+        # print(cls.get_synset_by_pos(word ,"NOUN"))
+
+wnu = WordNetUtils()
+print(wnu.get_synset("python"))
 
 
 class WordNetTagGenerator:
@@ -23,4 +93,4 @@ class WordNetTagGenerator:
             print(np.dot(vec[5], i) / np.sqrt(300))
             print(np.dot(vec[6], i) / np.sqrt(300))
 
-WordNetTagGenerator()
+# WordNetTagGenerator()
